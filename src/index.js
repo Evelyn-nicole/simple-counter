4709dc46afffd67ce7c8a992a7fd50d6c178d118
sebastian uribe osorio <evelyn.nicolee92@gmail.com>
2022-02-17 13:30:39 -0300

    simplecounter

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+let counter = 0;
+
+setInterval(() => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App counter={counter} />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  counter += 1;
+}, 1000);
+
+
